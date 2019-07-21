99e95fa7b353138571d402614f7c2b1df6cd4143
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-21 12:40:15 -0700

    I set up my app, model, route, and test, and I have a post route to create a tweet with a passing test

--- test/tweet.test.js
+++ test/tweet.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+// const Tweet = require('../lib/models/Tweet')
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,22 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({
+        handle: '@something-cool-as-hell',
+        text: 'I am cool as HELL!'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: '@something-cool-as-hell',
+          text: 'I am cool as HELL!',
+          __v: 0
+        });
+      });
   });
 });
